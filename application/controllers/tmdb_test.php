7eddf9ddb6c81933d93263e62a024b0dcb51969f
Chris Harvey <chrish@b2tm.com>
2011-02-17 00:04:06 +0000

    initial commit

---
+++ application/controllers/tmdb_test.php
@@ -0,0 +1,33 @@
+<?php
+
+class Tmdb_test extends CI_Controller {
+
+	function __construct()
+	{
+		parent::__construct();
+	}
+	
+	function index()
+	{
+		$this->load->library("TMDb");
+		
+		// Configurable Options
+		$api_key = 'APIKEY'; // Your TMDb API Key
+		$method = 'Movie.getInfo'; // A supported TMDb API method
+		$tmdb_movie_id = '105'; // A TMDb movie id (for use in this example)
+		
+		
+		$call = $this->tmdb->call($method, $api_key, array(1 => $tmdb_movie_id));
+		
+		echo "<pre>";
+		
+		echo "Name: ".$call->name;
+		echo "\nOverview: ".$call->overview;
+		
+		echo "</pre>";
+		
+		
+	}
+}
+
+/* End of file tmdb.php */
---
+++ application/libraries/TMDb.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * CodeIgniter TMDb (The Movie Database) API Library
+ * 
+ * Author: Chris Harvey (Back2theMovies)
+ * Website: http://www.b2tm.com
+ * Email: chrish@b2tm.com
+ *
+ * Originally developed for Back2theMovies (http://www.b2tm.com)
+ * 
+ **/
+ 
+class TMDb {
+
+	private $_api_url = 'http://api.themoviedb.org/2.1/';
+
+	private $_methods = array(
+						//	'Auth.getSession',
+						//	'Auth.getToken',
+						//	'Media.addID',
+							'Media.getInfo',
+						//	'Movie.addRating',
+							'Movie.browse',
+							'Movie.getImages',
+							'Movie.getInfo',
+							'Movie.getLatest',
+							'Movie.getTranslations',
+							'Movie.getVersion',
+							'Movie.imdbLookup',
+							'Movie.search',
+							'Person.getInfo',
+							'Person.getLatest',
+							'Person.getVersion',
+							'Person.search',
+							'Genres.getList'
+						);
+
+	public function call($method, $api_key, $options = array(), $params = NULL)
+	{
+		// Check the method exists
+		if(!in_array($method, $this->_methods))
+		{
+			return FALSE;
+		}
+
+		sort($options, SORT_NUMERIC); // Sort options into numerical order
+		
+		$options_string = '/en/json/'.$api_key; // Beginning of URL segments (Language, Type and API Key)
+		
+		foreach($options as $option){
+			$options_string .= '/'.$option;
+		}
+		
+		// Return the full URL string instead (for debugging purposes)
+		// return $this->_api_url.$method.$options_string;
+		
+		// Use cURL to call API and receive response
+		$ch = curl_init();
+
+		curl_setopt($ch, CURLOPT_URL, $this->_api_url.$method.$options_string);
+
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		
+		$response = curl_exec ($ch);
+
+		curl_close ($ch);
+		
+		$response = json_decode($response); // Decode the JSON response into an array
+		$response = $response[0]; // All data is contained in an unneccecary array (removed here)
+		
+		return $response; // Return the array
+		
+	}
+}
+/* End of file */
